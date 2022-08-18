91f9db8ee978db88943c5709946032dee8c9c360
Ricardo Silva <RickfSilva2020@users.noreply.github.com>
2022-08-18 17:09:14 -0300

    Update018/08,02

--- Telas/Sorteiok.py
+++ Telas/Sorteiok.py
@@ -2,83 +2,111 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 import random as rd
 from time import sleep as sl
 
 
 class Sorteio(BoxLayout):
     
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        pass
     
-        
-    def sequenciacheckbox_click(self, seq, value, *args):
-        
-        
-        
-        if value == True:
-            
-            self.ids.sequencia = True
-            self.ids.sequencia = seq
-            self.ids.output_label.text = 'Sequência selecionada'
-            
-     
-    def unicocheckbox_click(self, uni, value, *args):
-        
-        
-        
-        if value == True:
-            
-            self.ids.unico = True
-            self.ids.unico = uni
-            self.ids.output_label.text = 'Sorteio único selecionado' 
-            
+    
     def sorteio(self, *args):
+        
+        uni = self.ids.unico 
+        seq = self.ids.sequencia
         
         lista = []
         n = str(self.ids.element.text).upper().replace(' ', '_')
-
-
-
-        lista = []
         n1 = n.split()
 
         for i in n1:
             lista.append(i)
         
-        self.ids.output_label.text = f'Os elemanentos digitados foram {lista}.'
-
-
-        c = 0
-        if self.ids.unico.active:
+        if uni.active:
             
             res = rd.choice(lista)
             
-            print(res)
-          
+            box = BoxLayout(orientation='vertical')
+            box1 = BoxLayout(orientation='vertical')
+            botao1 = BoxLayout()
+            
+        
+            pop = Popup(title = 'Analizando resultado...', content = box, size_hint= (None, None),
+                    size=(300, 180), background_color=(0.3, 0.5, 1, 0.5))
+            pop2 = Popup(title = 'ESCOLHIDO', content = box1, size_hint= (None, None),
+                    size=(320, 180), background_color=(0.4, 0.5 , 0.7 ,1))
+            
+                 
+            ta = Button(text= 'VAI!!', on_release = pop2.open)
+            exit = Button(text= 'Exit',on_press = pop.dismiss,  on_release = pop2.dismiss)
+            texto = Label(text= 'Processando...')
+            texto2 = Label(text= 'O SORTEADO FOI...', font_size= 17)
+            texto3 = Label(text = f'{res}',color =(1,0,0,1), font_size= 25)
+            
+            botao1.add_widget(Label(text = ''))
+            botao1.add_widget(ta)
+            botao1.add_widget(Label(text = ''))
+            
+        
+            
+            
+            box.add_widget(texto)
+            box.add_widget(texto2)
+            box.add_widget(botao1)
+            
+            
+            box1.add_widget(texto3)
+            box1.add_widget(exit)
+            
+            pop.open()
 
-
-        if self.ids.sequencia.active:
-
+        c = 1
+        if seq.active:
+            
             res = rd.sample(lista, len(lista))
+        
             for e in res:
+                             
+                    
+                box = BoxLayout(orientation='vertical')
+                box1 = BoxLayout(orientation='vertical')
+                botao1 = BoxLayout()
                 
-                self.ids.output_label.text = f'{e} Clique em ok para o próximo'
+                pop = Popup(title = 'Analizando sequência...', content = box, size_hint= (None, None),
+                    size=(300, 180), background_color=(0.3, 0.5, 1, 0.5))
+                
+                pop1 = Popup(title = '', content = box1, size_hint= (None, None),
+                    size=(300, 180), background_color=(0.3, 0.5, 1, 0.5))
+                            
+                ss = Button(text= 'Vai', on_release = pop.dismiss)
+                
+                box.add_widget(Label(text= f'{c}º - {e}'))
+                box.add_widget(ss)
+                
+                box1.add_widget(Label(text= f'{res}'))
+                box1.add_widget(Button(text='PRÓXIMO'))
+                
+                
+                
+                pop.open()
+            
                 c += 1
+                 
+            
+        pop1.open()
                 
-               # Popup(title= 'Processando!!', size_hint= (None, None), size= (200, 150) )
-                sl(2)
-               # Popup(title= 'O SORTEADO FOI...', size_hint= (None, None), size= (200, 150) )
+           
+            
+            
+            
+            
                 
-                sl(2)
-               # Popup(title= f'{c}º - {e}', font_size= 40 , content = box, size_hint= (None, None), size= (200, 150))
                
-                
 
-        else:
-            self.ids.output_label.text = 'Selecione o tipo de sorteio'
+        #else:
+            #self.ids.output_label.text = 'Selecione o tipo de sorteio'
             
 
 
