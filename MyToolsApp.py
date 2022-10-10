326a00384b8e0a3c47cb63ca3315437afa696cf2
RickfSilva2020 <rickfsilva2020@users.noreply.github.com>
2022-10-10 12:00:48 -0300

    update 10/10/2022

--- MyToolsApp.py
+++ MyToolsApp.py
@@ -150,9 +150,192 @@ class Sorteio(Screen):
                 c += 1
                 
 class Cmotores(Screen):
+    '''
     def Calc_motores(self,**kwargs):
         super().__init__(self, **kwargs)
         pass
+   '''     
+        
+    def press(self):
+        
+        #Código para cálculos relacionados á motores
+
+        #Cálculo de corrente
+        '''
+        Para calcular a corrente do motor será nacassário a coleta das seguintes 
+        informações:
+        
+        Fórmula = Motor trifásico
+        I = P (watts)/(V * 1.732050808 * cosFI * rend.)
+        1Cv = 736 watts
+        rendimento médio dos motores 85
+        Coseno FI médio = 0.85 
+
+        Fórmula de motor monofásico
+
+        I = P (watts) / (V * cosFI * rend)
+
+        
+        Tensão em Volts
+        Potência em CV ou Kw
+        
+        '''
+        mottri = self.ids.tri
+        motmono = self.ids.mono
+        # INFORMAR O TIPO DE MOTOR. TRIFÁSICO OU MONOFÁSICO
+
+        #from curses.ascii import isalnum
+
+          
+        if mottri.active:   
+            print('Modo trifásico ativado')
+            
+        elif motmono.active:
+            print('Modo monofásico ativado')
+                      
+        else:
+            print('Escolha entre Trifásico ou Mono')
+            #self.ids.textout.text ='Escolha entre Trifásico ou Mono'
+            
+              
+              
+        uni = str(self.ids.uni.values)
+            
+        #if uni == 'CV' or uni == 'KW' or uni == 'WATTS':
+        print(uni)
+                
+        #else: 
+          #  mostrar = self.ids.textout.text='Escolha uma unidade para o motor'
+                
+        '''        
+        while True:
+            
+                p = str(self.ids.pot)
+                if p.isalpha() == False:
+                    break
+                    
+                else:
+                    mostrar = self.ids.textout.text= 'Informe um valor válido'
+                    p = str(self.ids.pot)
+                    if p.isalpha() == False:
+                        break
+        while True:
+                
+            v = str(self.ids.volt)
+            if v.isalpha() == False:
+                break
+                    
+            else:
+                mostrar = self.ids.textout.text = 'Informe um valor válido'
+                v = str(self.ids.volt)
+                if v.isalpha() == False:
+                    break
+                
+        while True:
+            
+            i = str(input('INFORME A CORRENTE DO MOTOR: '))
+            if i.isalpha() == False:
+                break
+                
+            else:
+                print('Informe um valor válido')
+                i = str(input('INFORME A CORRENTE DO MOTOR: '))
+                if i.isalpha() == False:
+                    break
+                
+            
+        if mot == 'T':
+            if p == '':
+                i = float(i)   
+                v = int(v)
+                pw = i * v * 1.732050808 * 0.85 * 0.85
+                pcv = pw / 736
+                if uni == 'CV':
+                    
+                    print(f'A potência do MIT é de {pcv:.2f} CV')
+                    
+                if uni == 'KW' or uni == 'W':
+                    
+                    print(f'A potência do MIT é de {pw / 1000:.2f} Kw')
+            elif v == '':
+                i = float(i)
+                p = float(p)
+                if uni == 'CV':
+                    pw = p * 736
+                    vc = pw / (i * 1.732050808 * 0.85 * 0.85)
+                    print(f'A tensão do MIT é {vc:.0f} Volts')
+                elif uni == 'KW':
+                    pw = p * 1000
+                    vc = pw / (i * 1.732050808 * 0.85 * 0.85)
+                    print(f'A tensão do MIT é {vc:.0f} Volts')
+                elif  uni == 'W':
+                    pw = p
+                    vc = pw / (i * 1.732050808 * 0.85 * 0.85)
+                    print(f'A tensão do MIT é {vc:.0f} Volts')
+            elif i == '':
+                v = int(v)
+                p = float(p)
+                if uni == 'CV':
+                    pw = p * 736
+                    i = pw / (v * 1.732050808 * 0.85 * 0.85 )
+                    print(f'A corrente do MIT é {i:.2f} Amperes')
+                elif uni == 'KW':
+                    pw = p * 1000
+                    i = pw / (v * 1.732050808 * 0.85 * 0.85 )
+                    print(f'A corrente do MIT é {i:.2f} Amperes')
+                elif uni == 'W':
+                    pw = p 
+                    i = pw / (v * 1.732050808 * 0.85 * 0.85 )
+                    print(f'A corrente do MIT é {i:.2f} Amperes')
+                        
+        if mot == 'M':
+            if p == '':
+                i = float(i)   
+                v = int(v)
+                pw = i * v * 0.85 * 0.85
+                pcv = pw / 736
+                if uni == 'CV':
+                    
+                    print(f'A potência do MIM é de {pcv:.2f} CV')
+                    
+                if uni == 'KW' or uni == 'W':
+                    
+                    print(f'A potência do MIM é de {pw / 1000:.2f} Kw')
+            elif v == '':
+                i = float(i)
+                p = float(p)
+                if uni == 'CV':
+                    pw = p * 736
+                    vc = pw / (i * 0.85 * 0.85)
+                    print(f'A tensão do MIM é {vc:.0f} Volts')
+                elif uni == 'KW':
+                    pw = p * 1000
+                    vc = pw / (i * 0.85 * 0.85)
+                    print(f'A tensão do MIM é {vc:.0f} Volts')
+                elif  uni == 'W':
+                    pw = p
+                    vc = pw / (i * 0.85 * 0.85)
+                    print(f'A tensão do MIM é {vc:.0f} Volts')
+            elif i == '':
+                v = int(v)
+                p = float(p)
+                if uni == 'CV':
+                    pw = p * 736
+                    i = pw / (v * 0.85 * 0.85 )
+                    print(f'A corrente do MIM é {i:.2f} Amperes')
+                elif uni == 'KW':
+                    pw = p * 1000
+                    i = pw / (v * 0.85 * 0.85 )
+                    print(f'A corrente do MIM é {i:.2f} Amperes')
+                elif uni == 'W':
+                    pw = p 
+                    i = pw / (v * 0.85 * 0.85 )
+                    print(f'A corrente do MIM é {i:.2f} Amperes')
+'''
+
+
+
+
         
     def escolhaPotencia(self):
         potencia = self.ids.pot.text
